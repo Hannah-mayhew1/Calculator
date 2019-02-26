46ae591870745afd8351253371f7d11064d6bd34
Hannah.Mayhew <hannah.mayhew@HannahMayhews-MacBook-Pro.local>
2019-02-26 12:23:27 +0000

    User number input

--- src/hannahmayhew/Calculator/Main.java
+++ src/hannahmayhew/Calculator/Main.java
@@ -1,41 +1,35 @@
 package hannahmayhew.Calculator;
 
 import java.util.Scanner;
-import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Calculator");
+            System.out.println("Welcome to the Calculator");
+            while (true) {
+            Scanner scanner = new Scanner(System.in);
 
-        Random random = new Random();
-        int first = random.nextInt(100);
-        int second = random.nextInt(100);
+            System.out.println("Enter your first number:");
+            int first = scanner.nextInt();
 
-        System.out.println("Your first number is: " + first);
-        System.out.println("Your second number is: " + second);
+            System.out.println("Choose an operation: +/*/-");
+            String input = scanner.next();
+            int answer;
 
-        System.out.println("Choose an operation: +/*/-");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        String add = "+";
-        String subtract = "-";
-        String multiply = "*";
-        int answer = 0;
+            System.out.println("Enter your second number:");
+            int second = scanner.nextInt();
 
-        if (input.equals(add)) {
-            answer = first + second;
+            if (input.equals("+")) {
+                answer = first + second;
+            } else if (input.equals("*")) {
+                answer = first * second;
+            } else if (input.equals("-")) {
+                answer = first - second;
+            } else {
+                System.out.println("Invalid operation");
+                return;
+            }
+
+            System.out.println("The answer is: " + answer);
         }
-        else if (input.equals(multiply)) {
-            answer = first * second;
-        }
-        else if (input.equals(subtract)) {
-            answer = first - second;
-        }
-        else {
-            System.out.println("Invalid operation");
-            return;
-        }
-
-        System.out.println("The answer is: " + answer);
     }
 }
